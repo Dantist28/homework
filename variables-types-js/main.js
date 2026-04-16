b144703693c932143940f143bf68978c353ce58e
Dantist28 <153408526+Dantist28@users.noreply.github.com>
2026-04-16 07:37:25 +0300

    Add files via upload

---
+++ variables-types-js/main.js
@@ -0,0 +1,33 @@
+//задача 1.
+
+const FIRST_NAME = "Роман";
+const LAST_NAME = "Слюсарчук";
+const IS_STUDENT = true;
+
+//задача 2.
+
+const AGE = 30;
+const CURRENT_YEAR = 2026;
+const BIRTH_YEAR = CURRENT_YEAR - AGE;
+
+//задача 3.
+
+console.log(
+  "Меня зовут " +
+    FIRST_NAME ,
+    LAST_NAME +
+    ", мне " +
+    AGE +
+    " лет. Я ученик курса: " +
+    IS_STUDENT,
+);
+
+// Задача 4.
+// Какое значение будет у переменной result?
+// let a = '123'; строка
+// let b = +'456'; число
+// let c = Number('789'); число 
+// let d = Boolean(0); false
+// let e = Boolean(' '); true
+// let result = a + b + c + d + e; 
+// result = 123456789falsetrue ; строка
